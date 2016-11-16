2199dbdc57ae17bd1821012fe32629fe475f9a4a
cgrange <coltongrange@hotmail.com>
2016-11-16 10:20:56 -0700

    adding markers on click?

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ function initMap() {
 //    scrollwheel: false,
     zoom: 14
   });
+  myMap = map;
+
+  map.addListener('click', function(e) {
+  	addMarker(e.latLng, map);
+  });
 }
+
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyBZrlFM-UWvijdFWL6aBYWaARHvAbd8Yeo",
@@ -16,3 +22,13 @@ var config = {
   messagingSenderId: "680615541265"
 };
 firebase.initializeApp(config);
+
+
+
+function addMarker(var myLatLng, var map){ // myLatLng: {lat: 40.2501, lng: -111.649}
+	var marker = new google.maps.Marker({
+		position: myLatLng,
+		map: map,
+		title: 'Hello World!'
+	});
+}
